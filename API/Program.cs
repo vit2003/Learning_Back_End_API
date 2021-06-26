14aa9b21f83255c9f46b1ff418b1f4fc6bb55616
vinh le <vinhle2311@gmail.com>
2021-06-26 19:35:57 +0700

    inital commit

---
+++ API/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Persistance;
+using System;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //handle all response for update migrate file.
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                }
+                catch (Exception e)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(e, "An error occur during migration");
+                }
+            }
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ Persistance/DataContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistance
+{
+    public class DataContext : DbContext 
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Value { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>().HasData(
+                new Value
+                {
+                Id = 1,
+                Name = "Value 101"
+                },   new Value
+                {
+                    Id = 2,
+                    Name = "Value 102"
+                },   new Value
+                {
+                    Id = 3,
+                    Name = "Value 103"
+                });
+        }
+    }
+}
