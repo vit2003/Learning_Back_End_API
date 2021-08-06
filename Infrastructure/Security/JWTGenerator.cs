10dfa73027605578dbb2e8e6df79301d0387c2ec
vinh le <vinhle2311@gmail.com>
2021-08-06 15:36:12 +0700

    Complele Login, Regis, GetCurrentUser

---
+++ Infrastructure/Security/JWTGenerator.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Application.Interfaces;
+using Domain;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Infrastructure.Security
+{
+    public class JWTGenerator : IJWTGenerator
+    {
+        private readonly IConfiguration configuration;
+        public JWTGenerator(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public string CreateToken(AppUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            //generate signing credentials
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["TokenKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
